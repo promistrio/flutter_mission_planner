44a134fe441280c239ff75bff5f369b3a0410c24
Vladislav <vlad@buzdin.ru>
2021-05-10 21:33:24 +0300

    init  with working map

---
+++ android/app/src/main/kotlin/com/example/airspector_mission_planner/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.airspector_mission_planner
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/bloc/mission_manager.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
+import 'package:flutter/material.dart';
+
+class MissionManager with ChangeNotifier, DiagnosticableTreeMixin {
+
+  List<Marker> _missionMarkers = [];
+
+  List<Marker> get missionMarkers => _missionMarkers;
+
+  void addWaypoint(MapController controller) {
+    controller.onReady.then((result) {
+      controller.move(LatLng(51.5, -0.09), 13.0);
+    });
+    notifyListeners();
+    _missionMarkers.add(
+      Marker(
+        width: 30.0,
+        height: 30.0,
+        point: controller.center,
+        builder: (ctx) => Container(
+            child: Column(
+          children: <Widget>[
+            //Text('${context.watch<Counter>().count}'),
+            Icon(Icons.control_point),
+          ],
+        )),
+      ),
+    );
+  }
+
+  /*/// Makes `Counter` readable inside the devtools by listing all of its properties
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(IntProperty('count', count));
+  }*/
+}
